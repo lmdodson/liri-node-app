67326be49259987b4f02c04bfd980bd86d67181d
Leah Dodson <leahdodson9@gmail.com>
2020-02-15 20:45:21 -0600

    added working omdb api code

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-// console.log("this is loaded");
+console.log("this is loaded");
 
 exports.spotify = {
 	id: process.env.SPOTIFY_ID,
 	secret: process.env.SPOTIFY_SECRET
 };
+
+exports.events = {
+	id: "codingbootcamp"
+}
+
+exports.omdb = {
+	id: "trilogy"
+}
--- liri.js
+++ liri.js
@@ -1,48 +1,107 @@
 var dotenv = require("dotenv").config();
 // console.log(dotenv)
 
-var Spotify = require('node-spotify-api');
+var axios = require("axios");
 
+var Spotify = require("node-spotify-api");
 // connects to the keys file
 var keys = require("./keys.js");
 
 // pull in user input
 var userCommand = process.argv[2];
-var userItem = process.argv.slice(3);
+var userItem = process.argv.slice(3).join(" ");
 
-// !Spotify search
+//! Bands in town search
+
+//! Spotify search
 // calls the spotify key
 var spotify = new Spotify(keys.spotify);
-var spotifyThis = function (song) {
-    console.log("Looking for your song")
 
-    spotify.search({
-        type: 'track',
-        query: song,
-        limit: 1
-    }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
+//Spotify search function
+var spotifyThis = function(song) {
+	if (!userItem) {
+		song = "Never Gonna Give You Up";
+	}
 
-        console.log(data.tracks.items[0].album.artists[0].name);
-    });
-}
+	console.log("Looking for your song");
 
-// spotifyThis(song)
+	// Spotify api call
+	spotify.search(
+		{
+			type: "track",
+			query: song,
+			limit: 1
+		},
+		function(err, data) {
+			if (err) {
+				return console.log("Error occurred: " + err);
+			}
+
+			var songData = [
+				"Song: " + song,
+				"Artist(s): " + data.tracks.items[0].artists[0].name,
+				"Album Name: " + data.tracks.items[0].album.name
+			].join("\n\n");
+			console.log(songData);
+		}
+	);
+};
+
+//! OMDB search
+var movieThis = function(movie) {
+	axios
+		.get(
+			"http://www.omdbapi.com/?t=" +
+				movie +
+				"&y=&plot=short&apikey=trilogy"
+		)
+		.then(function(response) {
+			var movieData = [
+				"Movie Title: " + movie,
+				"Release Date: " + response.data.Released,
+				"IMDB Rating: " + response.data.imdbRating,
+				"Rotten Tomatoes Rating: " + response.data.Ratings[1],
+				"Production Country: " + response.data.Country,
+				"Movie Language(s): " + response.data.Language,
+				"Plot: " + response.data.Plot,
+				"Actors: " + response.data.Actors
+			].join("\n\n");
+			console.log(movieData);
+		})
+		.catch(function(error) {
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				console.log("---------------Data---------------");
+				console.log(error.response.data);
+				console.log("---------------Status---------------");
+				console.log(error.response.status);
+				console.log("---------------Status---------------");
+				console.log(error.response.headers);
+			} else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an object that comes back with details pertaining to the error that occurred.
+				console.log(error.request);
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				console.log("Error", error.message);
+			}
+			console.log(error.config);
+		});
+};
 
 //! Switch case function for user input
 switch (userCommand) {
-    case "concert-this":
-        concertThis(userItem);
-        break;
-    case "spotify-this-song":
-        spotifyThis(userItem);
-        break;
-    case "movie-this":
-        movieThis(userItem);
-        break;
-    case "do-what-it-says":
-        doItFunction(userItem);
-        break;
-};
+	case "concert-this":
+		concertThis();
+		break;
+	case "spotify-this-song":
+		spotifyThis(userItem);
+		break;
+	case "movie-this":
+		movieThis(userItem);
+		break;
+	case "do-what-it-says":
+		doItFunction(userItem);
+		break;
+}
